47133e9ca284a4b96662ed6e168f2973ae10667d
Leo Arthur Cabral <109047018+Kingking11xD@users.noreply.github.com>
2026-09-21 17:25:08 -0700

    Delete app/main.py

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-from app.api.routes.health import router as health_router
-from app.api.routes.restaurants import router as restaurants_router
-
-app = FastAPI()
-
-app.include_router(health_router)
-app.include_router(restaurants_router)
