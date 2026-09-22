40cad13f3e24ad2c5b7f3466e302b59a0f1e24fb
leyy7a <leyl7aa@gmail.com>
2026-09-22 00:35:55 -0700

    fixed restaurant route

--- app/api/routes/restaurants.py
+++ app/api/routes/restaurants.py
@@ -1,9 +1,15 @@
 from fastapi import APIRouter
 
 from app.schemas.restaurant import Restaurant
+from app.repositories.restaurant_repo import RestaurantRepo
+from app.services.restaurant_service import RestaurantService
 
 router = APIRouter()
 
+repository = RestaurantRepo("data/restaurants.json")
+service = RestaurantService(repository)
+
+
 @router.get("/restaurants", response_model=list[Restaurant])
 def get_restaurants():
-    return []
+    return service.get_all_restaurants()
